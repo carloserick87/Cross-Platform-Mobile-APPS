1aa452de153b69af808f09f15d0a533f130694b9
Carlos Erick <ing.carloserick87@gmail.com>
2025-11-11 19:54:33 +0000

    fix: adjusted Vercel deployment configuration

--- Project_2/Act_2/src/main.jsx
+++ Project_2/Act_2/src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/app.router";
-import { SpeedInsights } from "@vercel/speed-insights/react"; // ✅ versión correcta para React
+import { SpeedInsights } from "@vercel/speed-insights/react"; 
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
