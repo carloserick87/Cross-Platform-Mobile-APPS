aabf2f04de495b47e11fe687bfe54d28b6ad514a
Carlos Erick <ing.carloserick87@gmail.com>
2025-11-11 19:43:23 +0000

    chore: integrate Vercel Speed Insights for performance monitoring

--- Project_2/Act_2/src/main.jsx
+++ Project_2/Act_2/src/main.jsx
@@ -1,12 +1,15 @@
-// src/main.jsx
+// Main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/app.router";
+import { SpeedInsights } from "@vercel/speed-insights/react"; // ✅ versión correcta para React
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <SpeedInsights /> 
   </StrictMode>
 );
+
