cd0e31cbda477dfa5457671b11c59b09f53b027f
Carlos Erick <ing.carloserick87@gmail.com>
2025-11-11 20:22:15 +0000

    chore: install @vercel/analytics dependency

--- Project_2/Act_2/src/main.jsx
+++ Project_2/Act_2/src/main.jsx
@@ -4,12 +4,14 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/app.router";
 import { SpeedInsights } from "@vercel/speed-insights/react"; 
+import { Analytics } from "@vercel/analytics/react"
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
     <SpeedInsights /> 
+    <Analytics />
   </StrictMode>
 );
 
