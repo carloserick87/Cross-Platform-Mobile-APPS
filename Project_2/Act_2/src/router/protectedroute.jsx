27f585c36b9bd4532ae577c4f9ef7ef7cc2b0c4d
Carlos Erick <ing.carloserick87@gmail.com>
2025-11-14 16:28:48 +0000

    Remove duplicate protectedroute.jsx file

--- Project_2/Act_2/src/router/protectedroute.jsx
+++
@@ -1,8 +0,0 @@
-// ProtectedRoute
-
-import { Navigate } from "react-router-dom";
-
-export default function ProtectedRoute({ children }) {
-  const user = JSON.parse(localStorage.getItem("registeredUser"));
-  return user ? children : <Navigate to="/register" replace />;
-}
